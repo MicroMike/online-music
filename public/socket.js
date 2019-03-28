dada2283cf9c7b5df13a74a503af6b12ec07477b
ASSOUNE Mike <massoune@nexity.fr>
2019-03-28 13:50:35 +0100

    c

--- public/socket.js
+++ public/socket.js
@@ -1,6 +1,17 @@
 var socket = io();
 
 socket.on('refresh', function (data) {
-  console.log(data)
-  document.querySelector('#messages').innerHTML = data
+  if (data.exit) {
+    document.querySelector('#list .' + data.account).remove()
+  }
+
+  if (data.used) {
+    document.querySelector('#list .' + data.account).remove()
+    document.querySelector('#used').insertAdjacentHTML('beforeEnd', '<div class="' + data.account + '">' + data.account + '</div>')
+  }
+
+  if (data.play) {
+    document.querySelector('#used .' + data.account).remove()
+    document.querySelector('#list').insertAdjacentHTML('beforeEnd', '<div class="' + data.account + '">' + data.account + '</div>')
+  }
 });
