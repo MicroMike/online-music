2cfe490f2ae48b78df154e14f35825eb6190624c
ASSOUNE Mike <massoune@nexity.fr>
2019-03-28 15:21:08 +0100

    c

--- public/socket.js
+++ public/socket.js
@@ -3,6 +3,10 @@ var socket = io();
 socket.on('refresh', function (data) {
   const className = data.account.replace(/[^a-z0-9]/gi, '')
 
+  if (data.accountsValid) {
+    document.querySelector('h1').innerText = data.accountsValid + ' accounts playing'
+  }
+
   if (data.exit) {
     document.querySelector('#list .' + className) && document.querySelector('#list .' + className).remove()
   }
