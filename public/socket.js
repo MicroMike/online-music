12978ed94dcb44bfb8a1933fe7d4c05ff037dd9d
ASSOUNE Mike <massoune@nexity.fr>
2019-03-28 14:38:17 +0100

    c

--- public/socket.js
+++ public/socket.js
@@ -2,16 +2,16 @@ var socket = io();
 
 socket.on('refresh', function (data) {
   if (data.exit) {
-    document.querySelector('#list .' + data.account).remove()
+    document.querySelector('#list #' + data.account).remove()
   }
 
   if (data.used) {
-    document.querySelector('#list .' + data.account).remove()
-    document.querySelector('#used').insertAdjacentHTML('beforeEnd', '<div class="' + data.account + '">' + data.account + '</div>')
+    document.querySelector('#list #' + data.account).remove()
+    document.querySelector('#used').insertAdjacentHTML('beforeEnd', '<div id="' + data.account + '">' + data.account + '</div>')
   }
 
   if (data.play) {
-    document.querySelector('#used .' + data.account).remove()
-    document.querySelector('#list').insertAdjacentHTML('beforeEnd', '<div class="' + data.account + '">' + data.account + '</div>')
+    document.querySelector('#used #' + data.account).remove()
+    document.querySelector('#list').insertAdjacentHTML('beforeEnd', '<div id="' + data.account + '">' + data.account + '</div>')
   }
 });
