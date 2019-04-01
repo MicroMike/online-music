464a5e221f5bdb37911d03b96f2b4c67bdcc65b3
ASSOUNE Mike <massoune@nexity.fr>
2019-04-01 12:36:38 +0200

    c

--- public/socket.js
+++ public/socket.js
@@ -1,23 +1,23 @@
-var socket = io();
+// var socket = io();
 
-socket.on('refresh', function (data) {
-  const className = data.account.replace(/[^a-z0-9]/gi, '')
+// socket.on('refresh', function (data) {
+//   const className = data.account.replace(/[^a-z0-9]/gi, '')
 
-  if (data.accountsValid) {
-    document.querySelector('h1').innerText = data.accountsValid + ' accounts playing'
-  }
+//   if (data.accountsValid) {
+//     document.querySelector('h1').innerText = data.accountsValid + ' accounts playing'
+//   }
 
-  if (data.exit) {
-    document.querySelector('#list .' + className) && document.querySelector('#list .' + className).remove()
-  }
+//   if (data.exit) {
+//     document.querySelector('#list .' + className) && document.querySelector('#list .' + className).remove()
+//   }
 
-  if (data.used) {
-    document.querySelector('#list .' + className) && document.querySelector('#list .' + className).remove()
-    document.querySelector('#used').insertAdjacentHTML('beforeEnd', '<div id="' + className + '">' + data.time + ' ' + data.account + '</div>')
-  }
+//   if (data.used) {
+//     document.querySelector('#list .' + className) && document.querySelector('#list .' + className).remove()
+//     document.querySelector('#used').insertAdjacentHTML('beforeEnd', '<div id="' + className + '">' + data.time + ' ' + data.account + '</div>')
+//   }
 
-  if (data.play) {
-    document.querySelector('#used .' + className) && document.querySelector('#used .' + className).remove()
-    document.querySelector('#list').insertAdjacentHTML('beforeEnd', '<div id="' + className + '">' + data.time + ' ' + data.account + '</div>')
-  }
-});
+//   if (data.play) {
+//     document.querySelector('#used .' + className) && document.querySelector('#used .' + className).remove()
+//     document.querySelector('#list').insertAdjacentHTML('beforeEnd', '<div id="' + className + '">' + data.time + ' ' + data.account + '</div>')
+//   }
+// });
