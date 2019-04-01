03e8e44e36d233e1724e3984850ceeaf80177401
ASSOUNE Mike <massoune@nexity.fr>
2019-04-01 12:44:43 +0200

    c

--- index.js
+++ index.js
@@ -17,48 +17,54 @@ let accounts
 let file = process.env.FILE || 'napsterAccount.txt'
 
 io.on('connection', client => {
-  fs.readFile(file, 'utf8', async (err, data) => {
-    if (err) return console.log(err);
+  console.log('connected')
+  client.emit('done')
 
-    fs.readFile('napsterAccountDel.txt', 'utf8', async (err2, dataDel) => {
-      if (err2) return console.log(err2);
-
-      accounts = data.split(',')
-
-      dataDel = dataDel.split(',').filter(e => e)
-      accounts = accounts.filter(e => dataDel.indexOf(e) < 0)
-
-      if (process.env.TYPE) {
-        accounts = accounts.filter(m => m.split(':')[0] === process.env.TYPE)
-      }
-
-      accounts = process.env.RAND ? shuffle(accounts) : accounts
-      console.log(accounts.length)
-      io.emit('accounts', accounts);
-    })
-  });
-
-  client.on('usedAccount', account => {
-    accounts = accounts.filter(a => a !== account)
-    io.emit('updateAccounts', accounts)
-  });
-
-  client.on('unusedAccount', account => {
-    accounts.push(account)
-    io.emit('updateAccounts', accounts)
-  });
-
-  client.on('delete', account => {
-    fs.readFile('napsterAccountDel.txt', 'utf8', function (err, data) {
+  client.on('getAccounts', () => {
+    fs.readFile(file, 'utf8', async (err, data) => {
       if (err) return console.log(err);
-      data = data.split(',').filter(e => e)
-      data = data.filter(a => a !== account)
-      data.push(account)
-      fs.writeFile('napsterAccountDel.txt', data.length === 1 ? data[0] : data.join(','), function (err) {
-        if (err) return console.log(err);
-      });
+
+      fs.readFile('napsterAccountDel.txt', 'utf8', async (err2, dataDel) => {
+        if (err2) return console.log(err2);
+
+        accounts = data.split(',')
+
+        dataDel = dataDel.split(',').filter(e => e)
+        accounts = accounts.filter(e => dataDel.indexOf(e) < 0)
+
+        if (process.env.TYPE) {
+          accounts = accounts.filter(m => m.split(':')[0] === process.env.TYPE)
+        }
+
+        accounts = process.env.RAND ? shuffle(accounts) : accounts
+        console.log(accounts.length)
+        client.emit('accounts', accounts);
+      })
     });
+
+    io.on('usedAccount', account => {
+      accounts = accounts.filter(a => a !== account)
+      io.emit('updateAccounts', accounts)
+    });
+
+    io.on('unusedAccount', account => {
+      accounts.push(account)
+      io.emit('updateAccounts', accounts)
+    });
+
+    io.on('delete', account => {
+      fs.readFile('napsterAccountDel.txt', 'utf8', function (err, data) {
+        if (err) return console.log(err);
+        data = data.split(',').filter(e => e)
+        data = data.filter(a => a !== account)
+        data.push(account)
+        fs.writeFile('napsterAccountDel.txt', data.length === 1 ? data[0] : data.join(','), function (err) {
+          if (err) return console.log(err);
+        });
+      });
+    })
   })
 });
+
 
 server.listen(process.env.PORT || 3000);
