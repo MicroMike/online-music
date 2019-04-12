b473adf202bb9d6b959f9223a851b264d7b00bd2
ASSOUNE Mike <massoune@nexity.fr>
2019-04-12 14:02:08 +0200

    c

--- index.js
+++ index.js
@@ -79,6 +79,7 @@ io.on('connection', client => {
 
   client.on('player', clientId => {
     isPlayer = true
+    console.log('play', clientId, clients[clientId])
     clients[clientId].emit('play')
   })
 
