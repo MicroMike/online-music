c233c8a9c12d77dba048198a2b4f2f7b91cab2cf
ASSOUNE Mike <massoune@nexity.fr>
2019-04-15 16:10:09 +0200

    c

--- index.js
+++ index.js
@@ -129,15 +129,12 @@ io.on('connection', client => {
 
     client.emit('goPlay')
   })
-  
 
   client.on('screen', data => {
     console.log(data.log)
     imgs.push(data)
-    imgs.forEach(d => {
-      Object.values(webs).forEach(c => {
-        c.emit('displayScreen', d)
-      })
+    Object.values(webs).forEach(c => {
+      c.emit('displayScreen', data)
     })
   })
 
