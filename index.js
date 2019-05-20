09bbc7c3fd86139a856637a50de0fcb140652e0f
ASSOUNE Mike <massoune@nexity.fr>
2019-05-20 10:49:56 +0200

    c

--- index.js
+++ index.js
@@ -73,22 +73,45 @@ let checking = false
 let plays = 0
 let nexts = 0
 
+let imgs = {}
+let clients = {}
+let streams = {}
+let webs = {}
+let checkClient
+
 const rand = (max, min) => {
   return Math.floor(Math.random() * Math.floor(max) + (typeof min !== 'undefined' ? min : 0));
 }
 
-const getAccount = env => {
-  if (env.RAND) {
-    for (let i = 0; i < accounts.length; i++) {
-      accounts.sort(() => { return rand(2) })
-    }
-  }
+const getAccount = async env => {
+  return new Promise(resolve => {
+    fs.readFile(file, 'utf8', async (err, data) => {
+      if (err) return console.log(err);
 
-  if (env.TYPE) {
-    accounts = accounts.filter(m => m.split(':')[0] === env.TYPE)
-  }
+      fs.readFile('napsterAccountDel.txt', 'utf8', async (err2, dataDel) => {
+        if (err2) return console.log(err2);
 
-  return accounts.length ? accounts.shift() : false
+        accounts = data.split(',')
+
+        dataDel = dataDel.split(',').filter(e => e)
+        accounts = accounts.filter(e => dataDel.indexOf(e) < 0)
+
+        Object.values(streams).forEach(s => accounts = accounts.filter(a => a !== s.account))
+
+        if (env.RAND) {
+          for (let i = 0; i < accounts.length; i++) {
+            accounts.sort(() => { return rand(2) })
+          }
+        }
+
+        if (env.TYPE) {
+          accounts = accounts.filter(m => m.split(':')[0] === env.TYPE)
+        }
+
+        resolve(accounts.length ? accounts.shift() : false)
+      })
+    });
+  })
 }
 
 let tempPlays = 0
@@ -97,31 +120,6 @@ setInterval(() => {
   gain = (plays - tempPlays) * 0.004
   tempPlays = plays
 }, 1000 * 60)
-
-const getAccounts = () => {
-  fs.readFile(file, 'utf8', async (err, data) => {
-    if (err) return console.log(err);
-
-    fs.readFile('napsterAccountDel.txt', 'utf8', async (err2, dataDel) => {
-      if (err2) return console.log(err2);
-
-      accounts = data.split(',')
-
-      dataDel = dataDel.split(',').filter(e => e)
-      accounts = accounts.filter(e => dataDel.indexOf(e) < 0)
-
-      console.log(accounts.length)
-    })
-  });
-}
-
-getAccounts()
-
-let imgs = {}
-let clients = {}
-let streams = {}
-let webs = {}
-let checkClient
 
 let displayLength = (log) => {
   const values = Object.values(streams)
@@ -171,20 +169,19 @@ io.on('connection', client => {
     client.uniqId = id
     streams[id] = client
 
-    accounts = accounts.filter(a => a !== account)
+    // accounts = accounts.filter(a => a !== account)
     // displayLength('Add')
 
     Object.values(webs).forEach(w => {
       w.emit('allData', getAllData())
     })
 
-    client.on('out', cid => {
-      const playerLength = Object.values(streams).filter(s => s.parentId === cid).length
-      const ok = !clients[cid] || playerLength >= clients[cid].max
-      console.log('out: ' + playerLength + ' ' + ok)
-      client.emit('outOk', ok)
-      // clients[cid] && clients[cid].emit('goPlay')
-    })
+    // client.on('out', cid => {
+    //   const playerLength = Object.values(streams).filter(s => s.parentId === cid).length
+    //   const ok = !clients[cid] || playerLength >= clients[cid].max
+    //   console.log('out: ' + playerLength + ' ' + ok)
+    //   client.emit('outOk', ok)
+    // })
 
     client.on('player', () => {
       // clients[client.parentId] && clients[client.parentId].emit('goPlay')
@@ -279,12 +276,15 @@ io.on('connection', client => {
           else { checking = false }
         }
         else {
-          const account = getAccount(env)
+          const account = await getAccount(env)
 
           if (account) {
             client.emit('run', account)
           }
         }
+      }
+      else {
+        Object.values(streams).filter(s => s.parentId === client.uniqId)[0].emit('out')
       }
     })
 
@@ -300,9 +300,12 @@ io.on('connection', client => {
       w.emit('allData', getAllData())
     })
 
+    if (client.account && accounts.indexOf(client.account) < 0) { accounts.push(client.account) }
+
     delete webs[client.id]
     delete clients[client.uniqId]
     delete streams[client.uniqId]
+    // delete imgs[client.uniqId]
 
     setTimeout(() => {
       restart = false
@@ -315,14 +318,7 @@ io.on('connection', client => {
     if (clients[client.uniqId]) {
       console.log('Disconnect Client ' + client.uniqId)
     }
-    else if (streams[client.uniqId]) {
-      if (code !== 4) {
-        if (accounts.indexOf(client.account) < 0) { accounts.push(client.account) }
-      }
-      if (code !== 100) {
-        // clients[client.parentId] && clients[client.parentId].emit('goPlay')
-      }
-    }
+    else if (streams[client.uniqId] && code !== 4) { }
 
     delete clients[client.uniqId]
     delete streams[client.uniqId]
@@ -360,7 +356,6 @@ io.on('connection', client => {
     })
 
     client.on('restart', cid => {
-      getAccounts()
 
       restart = true
       checking = false
