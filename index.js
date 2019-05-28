fcd406a881242e9862a2cf7726587eb01d24f6d7
ASSOUNE Mike <massoune@nexity.fr>
2019-05-28 12:00:22 +0200

    c

--- index.js
+++ index.js
@@ -299,7 +299,7 @@ io.on('connection', client => {
 
       if (!clients[client.uniqId]) { return }
 
-      c.playTimeout = setTimeout(() => {
+      client.playTimeout = setTimeout(() => {
         client.emit('goPlay')
       }, check ? 1000 * 60 : 1000 * 30 + rand(1000 * 90));
 
