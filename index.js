80913c501808ae9d56e53114f4e67edb49b1df4e
ASSOUNE Mike <massoune@nexity.fr>
2019-06-12 15:43:47 +0200

    c

--- index.js
+++ index.js
@@ -252,7 +252,7 @@ io.on('connection', client => {
       });
     })
 
-    runnerAccount && client.emit('streams', runnerAccount)
+    client.emit('streams', runnerAccount)
   })
 
   client.on('ok', async ({ accountsValid, del, max, env, first, id, check }) => {
