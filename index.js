041ab0d196953438e8caa032b0a6538352eb5bf0
ASSOUNE Mike <massoune@nexity.fr>
2019-07-02 11:00:09 +0200

    c

--- index.js
+++ index.js
@@ -202,7 +202,9 @@ io.on('connection', client => {
     client.on('playerInfos', datas => {
       resetTime && client.time < resetTime && client.emit('forceOut')
 
-      streams[datas.streamId].infos = datas
+      if (streams[datas.streamId]) {
+        streams[datas.streamId].infos = datas
+      }
 
       Object.values(webs).forEach(w => {
         // Object.values(streams).filter(s => !clients[s.parentId]).map(s => w.emit('playerInfos', { account: s.account, id: s.uniqId, nope: true }))
