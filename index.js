82437568280050f16df3792b99d9dbd9e20dffd2
ASSOUNE Mike <massoune@nexity.fr>
2019-07-03 16:14:39 +0200

    c

--- index.js
+++ index.js
@@ -115,8 +115,8 @@ const getAllData = () => ({
   nopeStreams: Object.values(streams).filter(s => s.parentId < resetTime).length,
   restart: restart || start,
   plays: plays * 0.004 + '€ (' + plays + ' / ' + nexts + ') ' + String(nexts / plays * 100).split('.')[0] + '%',
-  gain: gain + '€/min ' + String(gain * 60 * 24 * 30).split('.')[0] + '€/mois',
-  gain2: gain2 + '€/min ' + String(gain2 * 60 * 24 * 30).split('.')[0] + '€/mois',
+  gain: gain + '€/min ' + String(gain * 60 * 24).split('.')[0] + '€/jour' + String(gain * 60 * 24 * 30).split('.')[0] + '€/mois',
+  gain2: gain2 + '€/min ' + String(gain2 * 60 * 24).split('.')[0] + '€/jour' + String(gain2 * 60 * 24 * 30).split('.')[0] + '€/mois',
   clients: getNumbers(),
   errs: getErrs(),
 })
