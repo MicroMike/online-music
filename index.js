4c7d5e789db8c0b0d5cc1c760eb14e4ba6cc40f0
Unknown <massoune-ext@nexity.fr>
2019-08-27 17:25:08 +0200

    c

--- index.js
+++ index.js
@@ -172,7 +172,7 @@ io.on('connection', client => {
 
     loopInter = setInterval(() => {
       const running = Object.values(streams).filter(s => s.infos && s.infos.time && String(s.infos.time).match(/RUN|WAIT_PAGE/))
-      if (running && running.length === 0) { client.emit('run') }
+      if (!running || running.length === 0) { client.emit('run') }
     }, 1000 * 10)
   })
 
