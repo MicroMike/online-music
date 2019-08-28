f73bab5e8efe01542396df59069dd19e199d575b
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-08-28 14:26:13 +0200

    c

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ io.on('connection', client => {
     loopInter = setInterval(() => {
       const running = Object.values(streams).filter(s => s.parentId === id && s.infos && s.infos.time && String(s.infos.time).match(/RUN|WAIT_PAGE/)).length
       if (!running || running < 2) { client.emit('run') }
-    }, 1000 * 10)
+    }, 1000 * 30)
   })
 
   client.on('clearErrs', log => {
