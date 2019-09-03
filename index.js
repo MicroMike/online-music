0ce43898e144694ac4f1ce09bfb2ed85162ceed5
Assoune Mike <assoune.mike@gmail.com>
2019-09-03 23:05:38 +0200

    c

--- index.js
+++ index.js
@@ -210,7 +210,6 @@ io.on('connection', client => {
       const RUN_WAIT_PAGE = Object.values(streams).filter(s => s.parentId === parentId && s.infos && s.infos.time && String(s.infos.time).match(/RUN|WAIT_PAGE/)).length
       // const CONNECT = Object.values(streams).filter(s => s.parentId === id && s.infos && s.infos.time && String(s.infos.time).match(/CONNECT/)).length
 
-      console.log(getNumbers(parentId), max)
       if (!RUN_WAIT_PAGE && getNumbers(parentId) < Number(max)) { client.emit('run') }
     }, 1000 * 10)
   })
