ef48cc99b5768066454372f146eb0d9667398d5e
ASSOUNE Mike <massoune-ext@nexity.fr>
2019-09-03 11:24:46 +0200

    c

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ io.on('connection', client => {
     }
 
     client.loopInter = setInterval(() => {
-      const RUN_WAIT_PAGE = Object.values(streams).filter(s => s.parentId === parentId && s.infos && s.infos.time && String(s.infos.time).match(/RUN|WAIT_PAGE|CONNECT/)).length
+      const RUN_WAIT_PAGE = Object.values(streams).filter(s => s.parentId === parentId && s.infos && s.infos.time && String(s.infos.time).match(/RUN|WAIT_PAGE/)).length
       // const CONNECT = Object.values(streams).filter(s => s.parentId === id && s.infos && s.infos.time && String(s.infos.time).match(/CONNECT/)).length
 
       if (!RUN_WAIT_PAGE) { client.emit('run') }
