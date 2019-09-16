46e79711f091e2131a7e473011216204f3c381d9
Assoune Mike <massoune-ext@nexity.fr>
2019-09-16 16:21:05 +0200

    c

--- index.js
+++ index.js
@@ -160,8 +160,7 @@ const getAllData = () => ({
 const runLoop = (c, { parentId, env, max }) => {
   const RUN_WAIT_PAGE = Object.values(streams).filter(s => s.parentId === parentId && s.infos && s.infos.time && s.infos.other).length
 
-  if (getNumbers(parentId) < Number(max)) {
-    console.log(getNumbers(parentId), Number(max))
+  if (/*!parents[parentId].wait && !RUN_WAIT_PAGE && */getNumbers(parentId) < Number(max)) {
     const runnerAccount = env.CHECK ? checkAccounts.shift() : getAccount(env)
     if (!runnerAccount) { return }
 
