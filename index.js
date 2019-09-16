b28dadd962220a8a79d44d565b1f7c37c911947a
Assoune Mike <assoune.mike@gmail.com>
2019-09-16 22:35:26 +0200

    c

--- index.js
+++ index.js
@@ -209,7 +209,7 @@ io.on('connect', client => {
     client.wait = false
     client.inter = setInterval(() => {
       runLoop(client, { parentId, env, max })
-    }, 1000 + 30 + rand(1000 * 30 * Object.keys(parents).length));
+    }, 1000 + 30 + rand(1000 * 60 * 5));
 
     parents[parentId] = client
   })
