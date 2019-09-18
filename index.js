d505a85d7170c6ae341ef73fce24d5a2c4675ca5
Assoune Mike <massoune-ext@nexity.fr>
2019-09-18 14:32:55 +0200

    c

--- index.js
+++ index.js
@@ -168,7 +168,7 @@ const getAllData = () => ({
   gain: gain + '€/min ' + String(gain * 60 * 24).split('.')[0] + '€/jour ' + String(gain * 60 * 24 * 30).split('.')[0] + '€/mois',
   gain2: gain2 + '€/min ' + String(gain2 * 60 * 24).split('.')[0] + '€/jour ' + String(gain2 * 60 * 24 * 30).split('.')[0] + '€/mois',
   gain3: gain3 + '€/min ' + String(gain3 * 60 * 24).split('.')[0] + '€/jour ' + String(gain3 * 60 * 24 * 30).split('.')[0] + '€/mois',
-  clients: getNumbers(),
+  parents: getNumbers(),
   serverPlays: serverPlaysTemp,
   errs,
   parentsMax: maxs(),
