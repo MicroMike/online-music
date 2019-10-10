a9172216806e5cf47194fa96d9d60b057e51226b
Assoune Mike <massoune-ext@nexity.fr>
2019-10-10 11:30:02 +0200

    c

--- mongo.js
+++ mongo.js
@@ -231,7 +231,7 @@ module.exports = {
         const p = params && params.split('/')
         if (p) {
           MCard.deleteMany({})
-          const card = new MCard({ number: p[0], month: p[1], year: p[2], code: p[3] })
+          const card = new MCard({ cardNumber: p[0], cardMonth: p[1], cardYear: p[2], cardCode: p[3] })
           card.save((err, a) => { res.end(JSON.stringify(a)) })
 
         }
