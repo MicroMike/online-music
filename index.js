fa558bd391e47da249bd9be7cc6d554b2cd1f630
Assoune Mike <massoune-ext@nexity.fr>
2019-11-15 13:09:39 +0100

    c

--- index.js
+++ index.js
@@ -223,7 +223,7 @@ io.on('connect', client => {
   client.on('run', ({ parentId, env, max, back }) => {
     const RUN_WAIT_PAGE = Object.values(streams).filter(s => s.parentId === parentId && s.infos && s.infos.other).length
     back && console.log('back', parentId, getNumbers(parentId))
-    if ((!resultRatio[parentId] || resultRatio[parentId] >= 1) && !RUN_WAIT_PAGE && getNumbers(parentId) < max) {
+    if ((!resultRatio[parentId] || resultRatio[parentId] >= 0.5) && !RUN_WAIT_PAGE && getNumbers(parentId) < max) {
       const runnerAccount = env.CHECK ? checkAccounts && checkAccounts.shift() : getAccount(env)
       const streamId = rand(10000) + '-' + rand(10000) + '-' + rand(10000) + '-' + rand(10000)
 
