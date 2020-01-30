87ea6b329ed037c376f4a4248484f5a9e9b60882
Assoune Mike <massoune-ext@nexity.fr>
2020-01-30 14:29:44 +0100

    c

--- index.js
+++ index.js
@@ -219,6 +219,7 @@ const checkRun = (parentId, client, max, back) => {
   const RUN_WAIT_PAGE = Object.values(streams).filter(s => s.parentId === parentId && s.infos && s.infos.other).length
 
   if (!RUN_WAIT_PAGE && getNumbers(parentId) < max) {
+    streams[client.uniqId].infos = { time: 'CONNECT', other: true }
     client.emit(back ? 'mRun' : 'canRun')
   }
   else {
