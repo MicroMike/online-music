912ab2fe8f16cdbc7baa748bbb199e695edef0b0
Mike <assoune.mike@gmail.com>
2022-03-30 12:44:39 +0200

    c

--- index.js
+++ index.js
@@ -275,7 +275,8 @@ io.on('connect', client => {
 
 	client.on('run', ({ parentId, env, max, back }) => {
 		const RUN_WAIT_PAGE = Object.values(streams).filter(s => s.parentId === parentId && s.infos && s.infos.other).length
-		if (!atLeastOne || ((!resultRatio[parentId] || resultRatio[parentId] >= 0.5) && !RUN_WAIT_PAGE && getNumbers(parentId) < max)) {
+		// if (!atLeastOne || ((!resultRatio[parentId] || resultRatio[parentId] >= 0.5) && !RUN_WAIT_PAGE && getNumbers(parentId) < max)) {
+		if (!atLeastOne || (!RUN_WAIT_PAGE && getNumbers(parentId) < max)) {
 			const runnerAccount = env.CHECK ? checkAccounts && checkAccounts.shift() : getAccount(env)
 			const streamId = rand(10000) + '-' + rand(10000) + '-' + rand(10000) + '-' + rand(10000)
 
